83a8ea4882daa67c0505aa9d707a2a7b4c6fc576
anayarojo <raulmartin_04@hotmail.com>
2018-04-04 16:10:07 -0700

    Qualisys Test Service

---
+++ QualisysTestService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace QualisysTestService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QualisysTestProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.QualisysServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // QualisysTestProcessInstaller
+            // 
+            this.QualisysTestProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.QualisysTestProcessInstaller.Password = null;
+            this.QualisysTestProcessInstaller.Username = null;
+            // 
+            // QualisysServiceInstaller
+            // 
+            this.QualisysServiceInstaller.Description = "Qualisys Test Service";
+            this.QualisysServiceInstaller.DisplayName = "QualisysTestService";
+            this.QualisysServiceInstaller.ServiceName = "QualisysTestService";
+            this.QualisysServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.QualisysTestProcessInstaller,
+            this.QualisysServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller QualisysTestProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller QualisysServiceInstaller;
+    }
+}
---
+++ QualisysTestService/QualisysTestService.cs
@@ -0,0 +1,39 @@
+﻿using QualisysLog;
+using System;
+using System.ServiceProcess;
+
+namespace QualisysTestService
+{
+    partial class QualisysTestService : ServiceBase
+    {
+        public QualisysTestService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                QsLog.WriteInfo("Servicio Iniciado.");
+            }
+            catch (Exception lObjException)
+            {
+                QsLog.WriteException(lObjException);
+                base.Stop();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                QsLog.WriteInfo("Servicio Detenido.");
+            }
+            catch (Exception lObjException)
+            {
+                QsLog.WriteException(lObjException);
+            }
+        }
+    }
+}
